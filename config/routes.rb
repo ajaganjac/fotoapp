7c9c3d4cf46afbae684b80c888a2d48d9af8c687
Amer <amer.jaganjac@yahoo.com>
2018-01-19 20:30:48 -0500

    het werkt gewoon heel goed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :registrations => 'registrations' }
   root 'welcome#index'
+  
+  default_url_options :host => "https://ruby-on-rails--amerjaganjac831682.codeanyapp.com/"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
